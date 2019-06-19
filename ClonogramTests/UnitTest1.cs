3dead38be135f1d7237e78160fc1a865f2f9ffff
wownetort <fauust@gmail.com>
2019-06-19 21:42:15 +0300

    change

--- ClonogramTests/UnitTest1.cs
+++ ClonogramTests/UnitTest1.cs
@@ -34,7 +34,7 @@ namespace ClonogramTests
         [Fact]
         public async Task RedisYandex()
         {
-            var muxer = await ConnectionMultiplexer.ConnectAsync("rc1b-53udswomgfzm0jm3.mdb.yandexcloud.net:6379,password=Wfhmljns-2");
+            var muxer = await ConnectionMultiplexer.ConnectAsync("rc1b-53udswomgfzm0jm3.mdb.yandexcloud.net:26379,password=Wfhmljns-2");
             var conn = muxer.GetDatabase();
             _testOutputHelper.WriteLine(conn.Database.ToString());
         }
